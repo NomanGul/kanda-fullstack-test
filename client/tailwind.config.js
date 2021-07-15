ab608560a12956af10fa9d06511f1fe134a8f5ff
nomangul <nomangul2001@gmail.com>
2021-07-16 00:40:50 +0500

    :lipstick: use Nunito font-family as default sans font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+    },
     screens: {
       ...defaultTheme.screens,
       'sm': "575px",
